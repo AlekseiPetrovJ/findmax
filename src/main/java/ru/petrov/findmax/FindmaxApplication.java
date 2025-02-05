3f9b7d2e82a7fec3b083256329b189d51ae9658f
Aleksei Petrov <peta0982@gmail.com>
2025-02-05 21:04:43 +0500

    Initial commit

---
+++ src/main/java/ru/petrov/findmax/FindmaxApplication.java
@@ -0,0 +1,13 @@
+package ru.petrov.findmax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindmaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindmaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/petrov/findmax/FindmaxApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.petrov.findmax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindmaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
